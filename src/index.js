1f00895058b12631e2c0dcad9fff6c7cc7c84c26
necmettindemir <necmettindemir@yahoo.com>
2019-05-25 21:01:39 +0300

    ..

--- src/index.js
+++ src/index.js
@@ -1,12 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/* const App = () => 
+{
+
+    window.navigator.geolocation.getCurrentPosition(
+         (position) => console.log(position),
+         (err) => console.log(err) 
+    );
+
+    return <div>Lat : </div> 
+    
+} */
+
+class App extends React.Component {
+
+/*     constructor(props) {        
+        super(props);  
+        
+        //ONLY TIME we directly assign value to state
+        this.state = { lat: null, errorMessage:''};
+               
+    }
+ */
+
+    //SAME AS CALLING FROM CONSTRUCTOR
+    state = { lat: null, errorMessage:''};
+
+    componentDidMount() 
+    {
+    
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => 
+                    { 
+                        //console.log(position)
+                        //WE CALLED setState
+                        this.setState({lat:position.coords.latitude});
+                    },
+            (err) => {
+                        //console.log(err) ;
+                        this.setState({errorMessage:err.message});
+                    }
+       );
+
+    }
+
+
+    renderContent() {
+        /*  return (
+            <div>
+                <div>Lat : {this.state.lat} </div>
+                <div>{this.state.errorMessage} </div>
+            </div>            
+        ); */
+
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>err: {this.state.errorMessage}</div>;
+                   
+                
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            //return <div>Lat: {this.state.lat}</div>;
+            return  <SeasonDisplay lat={this.state.lat} />
+                    
+                 
+        } 
+
+        //return <div>Loading..</div>;
+        return <Spinner message="accept location request please.."/>;
+
+    }
+
+    render() {
+
+            return (
+                    <div className="border red">
+                        {this.renderContent()}
+                    </div>
+            );
+     
+    }
+}
+
+ReactDOM.render(
+    <App/>,document.querySelector('#root')
+);
